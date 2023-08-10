e0e8743cf66243f01eeb1f77a51a8fe4865f55e1
engvuchen <1742284391@qq.com>
2023-08-10 13:05:41 +0800

    U 打包时自动清空dist/assets

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from 'node:url';
+import { resolve } from 'node:path';
+import { rm } from 'node:fs/promises';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'Cleaning assets folder',
+      async buildStart() {
+        console.log('🔎 ~ file: vite.config.js:15 ~ buildStart ~ buildStart:');
+
+        await rm(resolve(__dirname, '../dist/assets'), { recursive: true, force: true });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
